aaced218a9c08ef8872625839676a0e24c961feb
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-08 14:11:03 +0100

    Update CertiStyle.jsx

--- src/conmponents/CertiStyle.jsx
+++ src/conmponents/CertiStyle.jsx
@@ -85,7 +85,7 @@ const FreeStyle = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2000,
   };
   return (
     <div className="container my-[9rem]">
